4e1b2c368513d275c72d481dcf7a8f3697371927
GDDMN <apawkin@gmail.com>
2024-06-28 13:47:07 +0300

    commits

--- Assets/Scripts/ItemGenerator/ItemGenerationSystem.cs
+++ Assets/Scripts/ItemGenerator/ItemGenerationSystem.cs
@@ -7,17 +7,17 @@ public class ItemGenerationSystem : IEcsRunSystem, IEcsInitSystem
 {
   private readonly EcsWorld _world = null;
   private readonly EcsFilter<ItemGeneratorComponent> _filter = null;
-
+  
   private float _time;
-  private Vector3 _spawnPoint;
+  //private Vector3 _spawnPoint;
   private int _itemsCount = 0;
+  private Transform _parent;
 
   public void Init()
   {
     foreach(var generator in _filter)
     {
       ref var generatorComponent = ref _filter.Get1(generator);
-      _spawnPoint = generatorComponent.SpawnPoint.position;
       _time = generatorComponent.GenerationTime;
     }
   }
@@ -27,10 +27,13 @@ public class ItemGenerationSystem : IEcsRunSystem, IEcsInitSystem
     foreach(var generator in _filter)
     {
       ref var generatorComponent = ref _filter.Get1(generator);
+
       List<ItemType> avaliableTypes = generatorComponent.AvaliableTypes;
+      Transform parent = generatorComponent.SpawnPoint;
       
       int maxItemsCount = generatorComponent.MaxItemsCount;
       generatorComponent.ItemsCount = _itemsCount;
+      Vector3 spawnPoint = generatorComponent.SpawnPoint.position;
 
       if (_itemsCount >= maxItemsCount)
         return;
@@ -43,24 +46,29 @@ public class ItemGenerationSystem : IEcsRunSystem, IEcsInitSystem
       ItemType randomType = (ItemType)UnityEngine.Random.Range(0, avaliableTypes.Count);
       ItemData data = Configs.Instance.GetItemConf(randomType).Data;
 
-      InitItem(data, _spawnPoint);
+      ItemComponent itemComponent = InitItem(data, spawnPoint, parent);
+
+      generatorComponent.SpawnPoint.position = spawnPoint;
+      generatorComponent.Items.Add(itemComponent);
 
       _time = generatorComponent.GenerationTime;
       _itemsCount++;
     }
   }
 
-  private void InitItem(ItemData data, Vector3 spawnPosition)
+  private ItemComponent InitItem(ItemData data, Vector3 spawnPosition, Transform parent)
   {
     GameObject prefab = Resources.Load<GameObject>(data.WayToPrefab);
     GameObject item = UnityEngine.Object.Instantiate(prefab, spawnPosition, Quaternion.identity);
+    item.transform.SetParent(parent);
 
     EcsEntity itemEntity = _world.NewEntity();
     SetCmponents(itemEntity);
 
     ref var itemComponent = ref itemEntity.Get<ItemComponent>();
+    spawnPosition = item.transform.GetChild(1).position;
 
-    _spawnPoint = item.transform.GetChild(1).position;
+    return itemEntity.Get<ItemComponent>();
   }
 
   private void SetCmponents(EcsEntity entity)
--- Assets/Scripts/ItemGenerator/ItemGeneratorComponent.cs
+++ Assets/Scripts/ItemGenerator/ItemGeneratorComponent.cs
@@ -7,6 +7,7 @@ public struct ItemGeneratorComponent
 {
   public float GenerationTime;
   public List<ItemType> AvaliableTypes;
+  public List<ItemComponent> Items;
   public Transform SpawnPoint;
   public int MaxItemsCount;
   [HideInInspector] public int ItemsCount;
